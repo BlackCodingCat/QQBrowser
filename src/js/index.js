5772473839baa4a86994b9153060d32445ad9d32
blackCodingCat <2569678842@qq.com>
2019-03-30 18:41:57 +0800

    完成了从空中落地的效果

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,9 @@ $(function(){
         navigation: true,
 	    navigationPosition: 'right',
 	    navigationTooltips: ['第一屏', '第二屏','第三屏','第四屏','第五屏'],
-        scrollingSpeed: 1000
+        scrollingSpeed: 1000,
+        onLeave: function(prev,next){
+           $('.section').eq(next.index).removeClass('current').siblings().addClass('current');
+        },
     })
 })
